4368e0c2f11aec971dca2ebea93f4829044f4ffa
seungchan baek <poketsc@gmail.com>
2021-05-05 23:20:39 +0900

    Refactor: 리뷰 기능 수정 완료 -> reservation 중복 코드 제거 완료 reviews/views.py -> reservation 중복 코드 제거

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,288 @@
-from django.test import TestCase
+import my_settings
+import bcrypt
+import jwt
+import json
 
-# Create your tests here.
+from django.test         import TestCase, Client
+from unittest.mock       import patch, MagicMock
+from django.core.files   import File
+
+from users.models        import User
+from hotels.models       import Category, Location, Hotel, Room
+from reservations.models import Reservation, Status
+from reviews.models      import Review
+
+class ReviewTest(TestCase):
+    def setUp(self):
+        
+        Category.objects.create(
+            id   = 1,
+            name = '호텔'
+        )
+
+        Location.objects.create(
+            id   = 1,
+            name = '강남구'
+        )
+
+        User.objects.create(
+            id           = 1,
+            email        = 'qwer@gmail.com',
+            password     = bcrypt.hashpw('qwerqwerQWER!'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            nickname     = 'zzang',
+            phone_number = '01012345678',
+            is_social    = 'False'
+        )
+
+        self.token1 = jwt.encode({'id':1}, my_settings.SECRET['secret'], algorithm=my_settings.ALGORITHM)
+
+
+        User.objects.create(
+            id           = 2,
+            email        = 'qwer2@gmail.com',
+            password     = bcrypt.hashpw('qwerqwerQWER!'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            nickname     = 'zzang2',
+            phone_number = '01012345679',
+            is_social    = 'False'
+        )
+
+        self.token2 = jwt.encode({'id':2}, my_settings.SECRET['secret'], algorithm=my_settings.ALGORITHM)
+
+        Hotel.objects.create(
+            id              = 1,
+            name            = '최고호텔',
+            address         = '강남구',
+            thumbnail_image = 'qwerqwerqwer',
+            longitude       = 12.21341231324,
+            latitude        = 14.2345234524352345,
+            category_id     = 1,
+            location_id     = 1,
+            star            = 5
+
+        )
+
+        Room.objects.create(
+            id             = 1,
+            name           = '스위트룸',
+            image_url      = 'asdfasdfasdfasdf',
+            original_price = 12000,
+            discount_rate  = 0.05,
+            occupancy      = 3,
+            hotel_id       = 1,
+        )
+
+        Status.objects.create(
+            id     = 2,
+            status = '숙박완료'
+        )
+        
+        Reservation.objects.create(
+            id           = 1,
+            name         = 'David',
+            phone_number = '01012345678',
+            check_in     = '2021-05-20',
+            check_out    = '2021-05-22',
+            status_id    = 2,
+            user_id      = 1,
+            hotel_id     = 1,
+            room_id      = 1,
+        )
+        
+        Review.objects.create(
+            id             = 1,
+            content        = '최고에요',
+            rate           = 10.00,
+            user_id        = 1,
+            hotel_id       = 1,
+            reservation_id = 1,
+        )
+    
+    def tearDown(self):
+        Review.objects.all().delete()
+        Reservation.objects.all().delete()
+        Status.objects.all().delete()
+        Room.objects.all().delete()
+        Hotel.objects.all().delete()
+        User.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+    
+    def test_post_upload_no_file_success_ReviewView(self):
+        client = Client()
+
+        headers   = {'HTTP_Authorization' : self.token1}
+        body      = json.dumps({'content':'좋아요', 'rate':10.00})
+        file_dict = {'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{'MESSAGE':'SUCCESS'})
+
+    @patch("my_settings.S3_CLIENT")
+    def test_post_upload_one_file_success_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token1}
+
+        mock_file                      = MagicMock(spec=File)
+        mock_file.name                 = 'test1.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = json.dumps({'content' : '좋아요~','rate' : 10.00})
+        file_dict = {'files':mock_file, 'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{'MESSAGE':'SUCCESS'})
+    
+    @patch("my_settings.S3_CLIENT")
+    def test_post_upload_multiple_files_success_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token1}
+
+        mock_file1                     = MagicMock(spec=File)
+        mock_file2                     = MagicMock(spec=File)
+        mock_file1.name                = 'test1.jpg'
+        mock_file2.name                = 'test2.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = json.dumps({'content' : '좋아요~','rate' : 10.00})
+        file_dict = {'files':[mock_file1,mock_file2], 'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{'MESSAGE':'SUCCESS'})
+
+    @patch("my_settings.S3_CLIENT")
+    def test_post_HOTEL_DOES_NOT_EXIST_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token1}
+
+        mock_file1                     = MagicMock(spec=File)
+        mock_file2                     = MagicMock(spec=File)
+        mock_file1.name                = 'test1.jpg'
+        mock_file2.name                = 'test2.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = json.dumps({'content' : '좋아요~','rate' : 10.00})
+        file_dict = {'files':[mock_file1,mock_file2], 'json':body}
+
+        response  = client.post('/reviews/hotel/2', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(),{'MESSAGE':'HOTEL_DOES_NOT_EXIST'})
+    
+    @patch("my_settings.S3_CLIENT")
+    def test_post_UNAUTHORIZED_USER_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token2}
+
+        mock_file1                     = MagicMock(spec=File)
+        mock_file2                     = MagicMock(spec=File)
+        mock_file1.name                = 'test1.jpg'
+        mock_file2.name                = 'test2.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = json.dumps({'content' : '좋아요~','rate' : 10.00})
+        file_dict = {'files':[mock_file1,mock_file2], 'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),{'MESSAGE':'UNAUTHORIZED_USER'})
+    
+    @patch("my_settings.S3_CLIENT")
+    def test_post_KEY_ERROR_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token1}
+
+        mock_file1                     = MagicMock(spec=File)
+        mock_file2                     = MagicMock(spec=File)
+        mock_file1.name                = 'test1.jpg'
+        mock_file2.name                = 'test2.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = json.dumps({'content' : '좋아요~'})
+        file_dict = {'files':[mock_file1, mock_file2], 'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{'MESSAGE':'KEY_ERROR'})
+    
+    @patch("my_settings.S3_CLIENT")
+    def test_post_JSONDecodeError_ReviewView(self, mocked_s3client):
+        client  = Client()
+
+        headers = {'HTTP_Authorization' : self.token1}
+
+        mock_file1                     = MagicMock(spec=File)
+        mock_file2                     = MagicMock(spec=File)
+        mock_file1.name                = 'test1.jpg'
+        mock_file2.name                = 'test2.jpg'
+        mocked_s3client.upload_fileobj = MagicMock()
+
+        body      = {'content' : '좋아요~', 'rate' : 10.00}
+        file_dict = {'files':[mock_file1, mock_file2], 'json':body}
+
+        response  = client.post('/reviews/hotel/1', file_dict, **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{'MESSAGE':'JSON_DECODE_ERROR'})
+    
+    def test_get_success_ReviewView(self):
+        client   = Client()
+
+        response = client.get('/reviews/hotel/1')
+        review   = Review.objects.get(pk=1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{'RESULTS':[{
+            'content': '최고에요',
+            'rate': '10.00',
+            'rate_comment': '여기만한 곳은 어디에도 없을 거예요.',
+            'nickname': 'zzang',
+            'created_at': review.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'image_url': []}]})
+    
+    def test_get_HOTEL_DOES_NOT_EXIST_ReviewView(self):
+        client   = Client()
+
+        response = client.get('/reviews/hotel/2')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(),{'MESSAGE':'HOTEL_DOES_NOT_EXIST'})
+    
+    def test_delete_success_ReviewView(self):
+        client   = Client()
+
+        headers  = {'HTTP_Authorization' : self.token1}
+        response = client.delete('/reviews/1', **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{'MESSAGE':'SUCCESS'})
+    
+    def test_delete_REVIEW_DOES_NOT_EXIST_ReviewView(self):
+        client   = Client()
+
+        headers  = {'HTTP_Authorization' : self.token1}
+        response = client.delete('/reviews/2', **headers)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(),{'MESSAGE':'REVIEW_DOES_NOT_EXIST'})
+
+    def test_delete_UNAUTHORIZED_USER_ReviewView(self):
+        client   = Client()
+
+        headers  = {'HTTP_Authorization' : self.token2}
+        response = client.delete('/reviews/1', **headers)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),{'MESSAGE':'UNAUTHORIZED_USER'})
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+import json
+import boto3
+import my_settings
 
-# Create your views here.
+from django.views        import View
+from django.http         import JsonResponse
+
+from reviews.models      import Review, ReviewComment, ReviewImage
+from hotels.models       import Hotel, Room
+from users.models        import User
+from reservations.models import Reservation
+from users.utils         import LoginDecorator
+
+class ReviewView(View):
+    @LoginDecorator
+    def post(self, request, hotel_id):
+        try:
+            data        = json.loads(request.POST['json'])
+            content     = data['content']
+            rate        = data['rate']
+            files       = request.FILES.getlist('files', None)
+            s3_client   = my_settings.S3_CLIENT
+            user        = request.user
+            reservation = Reservation.objects.filter(user=user, hotel=hotel_id, status=2)
+            hotel_exist = Hotel.objects.filter(id=hotel_id).exists()
+
+            if not hotel_id or not hotel_exist:
+                return JsonResponse({'MESSAGE':'HOTEL_DOES_NOT_EXIST'}, status=404)
+            
+            if not reservation.exists():
+                return JsonResponse({'MESSAGE':'UNAUTHORIZED_USER'}, status=401)
+
+            review = Review.objects.create(
+                content     = content,
+                rate        = rate,
+                hotel_id    = hotel_id,
+                user        = user,
+                reservation = reservation.order_by('-created_at').first(),
+            )
+
+            for file in files:
+                s3_client.upload_fileobj(
+                    file,
+                    my_settings.AWS_S3_BUCKET_NAME,
+                    file.name,
+                    ExtraArgs = {
+                        "ContentType" : file.content_type
+                    }
+                )
+            
+            review.reviewimage_set.bulk_create([
+                ReviewImage(
+                image_url = f'https://{my_settings.AWS_S3_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/{file.name}',
+                review    = review
+                ) for file in files])
+            
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+            
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+    
+    def get(self, request, hotel_id):
+        hotel_exist = Hotel.objects.filter(id=hotel_id).exists()
+
+        if not hotel_id or not hotel_exist:
+            return JsonResponse({'MESSAGE':'HOTEL_DOES_NOT_EXIST'}, status=404)
+
+        reviews = Review.objects.filter(hotel=hotel_id)
+
+        results = [{
+            'content'      : review.content,
+            'rate'         : review.rate,
+            'rate_comment' : '여기만한 곳은 어디에도 없을 거예요.' if review.rate == 10.00 else '만족해요',
+            'nickname'     : review.user.nickname,
+            'created_at'   : review.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'image_url'    : [review_image.image_url for review_image in review.reviewimage_set.all()]
+        } for review in reviews]
+
+        return JsonResponse({'RESULTS':results}, status=200)
+    
+    @LoginDecorator
+    def delete(self, request, review_id):
+        review_exist = Review.objects.filter(id=review_id).exists()
+
+        if not review_id or not review_exist:
+            return JsonResponse({'MESSAGE':'REVIEW_DOES_NOT_EXIST'}, status=404)
+
+        user   = request.user
+        review = Review.objects.filter(user=user, id=review_id).exists()
+
+        if not review:
+            return JsonResponse({'MESSAGE':'UNAUTHORIZED_USER'}, status=401)
+            
+        Review.objects.get(user=user, id=review_id).delete()
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
