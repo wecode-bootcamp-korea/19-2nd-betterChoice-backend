07f382d33888b1afc4dab561cffd98a56da5cf5a
seungchan baek <poketsc@gmail.com>
2021-05-05 23:20:39 +0900

    Refactor: 리뷰 기능 수정 완료 -> reservation 중복 코드 제거 완료 reviews/views.py -> reservation 중복 코드 제거

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls   import path
 
+from reviews.views import ReviewView
 
 urlpatterns = [
-  
+    path('/hotel/<int:hotel_id>', ReviewView.as_view()),
+    path('/<int:review_id>', ReviewView.as_view())
 ]
